8e7236f359e8827ac39972ae1ec11852dc00d596
J-Herranz <joel.herranz@gmail.com>
2024-12-09 05:53:57 +0100

    Calculator app: adapts calculator grid to small landscape screens

--- src/apps/calculator/components/Calculator.jsx
+++ src/apps/calculator/components/Calculator.jsx
@@ -15,43 +15,45 @@ function Calculator() {
   return (
     <>
       <SubAppTitle />
-      <div className="calculator-grid">
-        <div className="output">
-          <div className="previous-operand">
-            {formatOperand({ operand: previousOperand })} {operation}
+      <div className="calculator-grid-container">
+        <div className="calculator-grid">
+          <div className="output">
+            <div className="previous-operand">
+              {formatOperand({ operand: previousOperand })} {operation}
+            </div>
+            <div className="current-operand">{formatOperand({ operand: currentOperand })}</div>
           </div>
-          <div className="current-operand">{formatOperand({ operand: currentOperand })}</div>
+          <button
+            className="calculator-button span-two"
+            onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+          >
+            AC
+          </button>
+          <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })} className="calculator-button">
+            DEL
+          </button>
+          <OperationButton operation="÷" dispatch={dispatch} />
+          <DigitButton digit="1" dispatch={dispatch} />
+          <DigitButton digit="2" dispatch={dispatch} />
+          <DigitButton digit="3" dispatch={dispatch} />
+          <OperationButton operation="*" dispatch={dispatch} />
+          <DigitButton digit="4" dispatch={dispatch} />
+          <DigitButton digit="5" dispatch={dispatch} />
+          <DigitButton digit="6" dispatch={dispatch} />
+          <OperationButton operation="+" dispatch={dispatch} />
+          <DigitButton digit="7" dispatch={dispatch} />
+          <DigitButton digit="8" dispatch={dispatch} />
+          <DigitButton digit="9" dispatch={dispatch} />
+          <OperationButton operation="-" dispatch={dispatch} />
+          <DigitButton digit="." dispatch={dispatch} />
+          <DigitButton digit="0" dispatch={dispatch} />
+          <button
+            className="calculator-button span-two"
+            onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+          >
+            =
+          </button>
         </div>
-        <button
-          className="calculator-button span-two"
-          onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-        >
-          AC
-        </button>
-        <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })} className="calculator-button">
-          DEL
-        </button>
-        <OperationButton operation="÷" dispatch={dispatch} />
-        <DigitButton digit="1" dispatch={dispatch} />
-        <DigitButton digit="2" dispatch={dispatch} />
-        <DigitButton digit="3" dispatch={dispatch} />
-        <OperationButton operation="*" dispatch={dispatch} />
-        <DigitButton digit="4" dispatch={dispatch} />
-        <DigitButton digit="5" dispatch={dispatch} />
-        <DigitButton digit="6" dispatch={dispatch} />
-        <OperationButton operation="+" dispatch={dispatch} />
-        <DigitButton digit="7" dispatch={dispatch} />
-        <DigitButton digit="8" dispatch={dispatch} />
-        <DigitButton digit="9" dispatch={dispatch} />
-        <OperationButton operation="-" dispatch={dispatch} />
-        <DigitButton digit="." dispatch={dispatch} />
-        <DigitButton digit="0" dispatch={dispatch} />
-        <button
-          className="calculator-button span-two"
-          onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
-        >
-          =
-        </button>
       </div>
     </>
   )
